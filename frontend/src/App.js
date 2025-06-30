cc66202203e0c34744bc48d32eb600ee100bd4fe
A-Rahman-19 <abdur.rahman19@outlook.com>
2025-06-30 19:40:25 +0530

    Added font awsome to webpage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,27 @@
 import logo2 from './assets/LogoWhite.png';
-import './App.css';
+import './App.css'; 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faInstagram, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 function App() {
   const currentYear = new Date().getFullYear();
   return (
     <>
-    <div className="container">
-      <img src={logo2} alt="Adflyn Logo" className="logo fade-in-logo" />
-      <h1 className='fade-in-text'>Adflyn: Simplify. Automate. Market.<br/>All in One Place</h1>
-      <p className="subtitle fade-in-subtext">We're building something powerful.<br/>Stay tuned.</p>
-      <a href="https://www.linkedin.com/company/adflyn/" target="_blank" className="linkedin" rel="noopener noreferrer">
-        <img src=" https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linkedin/linkedin-original.svg" alt="LinkedIn" className='fade-in-icon' />
-      </a>
-      
-    </div>
-    <footer className="footer">
+      <div className="container">
+        <img src={logo2} alt="Adflyn Logo" className="logo fade-in-logo" />
+        <h1 className='fade-in-text'>Adflyn: Simplify. Automate. Market.<br/>All in One Place</h1>
+        <p className="subtitle fade-in-subtext">We're building something powerful.<br/>Stay tuned.</p>
+        <a href="https://www.linkedin.com/company/adflyn/" target="_blank" className="socials" rel="noopener noreferrer">
+       <FontAwesomeIcon icon={faLinkedin} size="2x" color="white" className="fade-in-icon" />
+</a>
+        <a href="https://www.instagram.com/adflyn/" target="_blank" className="socials" rel="noopener noreferrer">
+         <FontAwesomeIcon icon={faInstagram} size="2x" color="white" className="fade-in-icon" />
+        </a>
+      </div>
+      <footer className="footer">
         &copy; {currentYear} Adflyn. All rights reserved  
       </footer>
-      </>
+    </>
   );
 }
 
