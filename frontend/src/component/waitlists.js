915c8fb33650d326d95c3d42d26a6f56ef10c36f
A-Rahman-19 <abdur.rahman19@outlook.com>
2025-07-29 02:51:56 +0530

    Added content

--- frontend/src/component/waitlists.js
+++ frontend/src/component/waitlists.js
@@ -1,8 +1,16 @@
+import './waitlists.css'; 
+
 const Waitlist = () => {
   return (
     <div>
-      <h2>Join the Waitlist</h2>
-      <p>We're excited to have you on board! Please fill out the form below to join our waitlist.</p>
+      <h2 className='fade-in-text'>You're Just in Time.</h2>
+      <p className="subtitle fade-in-subtext">
+        We’re on a mission to build a new kind of marketing system — one where <br/>
+        automation does the heavy lifting, and you stay in control.
+      </p>
+      <button className="waitlist-button1 fade-in-button1">
+        Join Waitlist!
+      </button>
     </div>
   );
 };
