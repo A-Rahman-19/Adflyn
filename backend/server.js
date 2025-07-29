318849bf8b8ae751127654a1590b494acc398d45
A-Rahman-19 <abdur.rahman19@outlook.com>
2025-07-29 07:04:59 +0530

    added cors

--- backend/server.js
+++ backend/server.js
@@ -6,8 +6,13 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
-app.use(cors());
 app.use(bodyParser.json());
+
+app.use(cors({
+  origin: 'https://www.adflyn.com',
+  methods: ['GET', 'POST', 'OPTIONS'],
+  credentials: true // optional: only if using cookies/auth
+}));
 
 const PORT = 8000;
 
